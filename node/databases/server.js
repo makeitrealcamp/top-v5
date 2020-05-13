477817079f12dc890542853a69d8f05b9c62c103
Simon Hoyos <shmesa23@gmail.com>
2020-05-13 10:17:43 -0500

    NodeJS controllers and routes

--- node/databases/server.js
+++ node/databases/server.js
@@ -2,10 +2,24 @@
 const express = require('express');
 const cors = require('cors');
 const initDatabase = require('./src/db.js');
+const userController = require('./src/controllers/user.controller');
 
 initDatabase();
 const app = express();
+app.use(cors());
+app.use(express.json());
 
+function greetController(req, res) {
+  res.send('hello world')
+}
+
+app.get('/greet', greetController);
+
+app.get('/users', userController.list);
+app.post('/users', userController.create);
+app.get('/users/:id', userController.show);
+app.put('/users/:id', userController.edit);
+app.delete('/users/:id', userController.destroy);
 
 
 app.listen(8080, () => console.log(`App running on http://localhost:8080`));
