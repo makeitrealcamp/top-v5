ba246b8bfa7529d98df97bb613758a7fc55c0b78
Simon Hoyos <shmesa23@gmail.com>
2020-05-22 16:10:34 -0500

    BEM

--- react/first-steps/src/App.js
+++ react/first-steps/src/App.js
@@ -4,24 +4,27 @@ import webpack from './assets/download.png';
 import './App.css';
 
 const name = 'Simon';
+// BEM - Block__Element--modifier
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App__header">
+        <img src={logo} className="App__logo" alt="logo" />
         <img src={webpack} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hola {name}, esta es mi primera aplicación con React</p>
+        <p className="App__paragraph">Hola {name}, esta es mi primera aplicación con React</p>
         <a
-          className="App-link"
+          className="App__link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <button className="App__button App__button--primary">Button 1</button>
+        <button className="App__button App__button--secondary App__button--red">Button 2</button>
       </header>
     </div>
   );
