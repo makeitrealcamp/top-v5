8b5496ca28f8850c13230421a35ca15336620e9d
Simon Hoyos <shmesa23@gmail.com>
2020-06-05 11:35:09 -0500

    File Upload frontend side

---
+++ files/frontend/src/App.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState(null);
+  const [file, setFile] = useState(null);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const url = await uploadFile(file);
+
+  }
+
+  async function uploadFile(file) {
+    const data = new FormData();
+    data.append('file', file, file.name);
+    data.append('upload_preset', 'lesson-preset');
+
+    const res = await axios({
+      method: 'POST',
+      baseURL: 'https://api.cloudinary.com/v1_1/simonmir',
+      url: '/image/upload',
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      data
+    });
+
+    return res.data.url;
+  }
+
+  function readFile(file) {
+    const reader = new FileReader();
+    reader.onload = e => setImage(e.target.result);
+    reader.readAsDataURL(file);
+  }
+
+  function handleImage(e) {
+    console.log(e.target.files[0])
+    readFile(e.target.files[0]);
+    uploadFile(e.target.files[0]);
+    // setFile(e.target.files[0]);
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor="name">name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={e => setName(e.target.value)}
+            value={name}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="file">name</label>
+          <input
+            type="file"
+            accept="image/*"
+            name="file"
+            id="file"
+            onChange={handleImage}
+          />
+        </fieldset>
+      </form>
+      <button>Create</button>
+      {image && (
+        <img
+          src={image}
+          alt="upload preview"
+          width="300"
+          height="300"
+        />
+      )}
+      <img src="https://res.cloudinary.com/simonmir/image/upload/v1591367476/mirlesson/t5w9llcxk6t8wuoestgu.jpg" />
+    </div>
+  );
+}
+
+export default App;
