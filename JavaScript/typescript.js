fa2ac002b54bafe96965f8421f8f2449315833aa
Simon Hoyos <shmesa23@gmail.com>
2020-07-07 09:38:13 -0500

    Typescript basics

---
+++ JavaScript/typescript.js
@@ -0,0 +1,32 @@
+// let num: number = 8;
+// num = 'string';
+// type barTypes = number | string | Array<string>
+// let bar: barTypes = 10;
+// bar = 'string';
+// bar = [1,2,3];
+// interface IPerson {
+//   name: string;
+//   lastname: string;
+//   age: number;
+//   working?: boolean;
+// }
+// const person: IPerson = {
+//   name: 'Maria',
+//   lastname: 'Hoyos',
+//   age: 28,
+// };
+// enum Name {
+//   Simon = 'Simon',
+//   Maria = 'Maria',
+//   Ana = 'Ana',
+//   Juan = 'Juan'
+// }
+// let name: Name = Name.Simon;
+// function sum(a: number, b: number): number {
+//   return a + b;
+// }
+// sum('string', 2);
+function log(text) {
+    console.log(text);
+}
+log('hola mundo');
